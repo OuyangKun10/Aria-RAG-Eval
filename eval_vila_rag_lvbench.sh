d10b36786b5e39d922f0f942fd292168d33ad56c
Ouyang Kun <131441216+OuyangKun10@users.noreply.github.com>
2025-03-16 21:58:06 +0800

    Add files via upload

--- eval_vila_rag_lvbench.sh
+++ eval_vila_rag_lvbench.sh
@@ -8,7 +8,7 @@ python3 -m accelerate.commands.launch \
     --num_processes 8 \
     -m lmms_eval \
     --model vila \
-    --model_args pretrained="Efficient-Large-Model/VILA1.5-40b",attn_implementation="flash_attention_2",max_frames_num=14,device_map="auto" \
+    --model_args pretrained="Efficient-Large-Model/VILA1.5-40b",attn_implementation="flash_attention_2",max_frames_num=14,device_map="auto",rag_file="/home/ouyangkun/lmmeval/rag_file/longvideobench_rag_val_mm_rag_0114_span_with_scores_index_148k_etbench_taskprompt_rag_44k_from_0916_256f_fps1.0_subtitle_parsed.json" \
     --tasks longvideobench_val_v \
     --batch_size 1 \
     --log_samples \
